26b88575d64b3598b446231a3f89ff2d540e26c9
Mirko Coric <mirkocoric@gmail.com>
2017-06-21 17:43:29 +0200

    Added multiple processes

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ clean:
 	rm -rf venv
 
 run:
-	@${PYTHONPATH} mutual_exclusion.py 10 3 'pingpong'
+	@${PYTHONPATH} mutual_exclusion.py 4 3 'pingpong' 1
 
 virtualenv: requirements.txt | venv 
 	@venv/bin/pip install -r requirements.txt
--- mutual_exclusion.py
+++ mutual_exclusion.py
@@ -3,20 +3,21 @@ import argparse
 from multiprocessing import Process
 from multiprocessing import Pipe
 from itertools import combinations
+import time
 
-def process(pid, connread, connwrite, numiter, method):
+def process(pid, connread, connwrite, numiter, method, duration):
     '''Executes process'''
     if method == 'pingpong':
-        pingpong(pid, connread, connwrite, numiter)
+        pingpong(pid, connread, connwrite, numiter, duration)
     #print(connections_send)
     #print(connections_recv)
     
 
-def create_processes(numiter, numprocesses, method, pipesread, pipeswrite):
+def create_processes(numiter, numprocesses, method, pipesread, pipeswrite, duration):
     '''Creates maxnum processes'''
     #'Definirati poslije je li treba koristiti jedan ili dva pipea'
     return [Process(target=process, args=(proc, pipesread[proc], pipeswrite[proc], numiter,
-                                          method))
+                                          method, duration))
             for proc in xrange(1, numprocesses + 1)]
 
 def create_pipes_all(numprocesses):
@@ -51,46 +52,56 @@ def create_pipes(numprocesses, method):
 #def ricart_argawala(number, pipes, numiter):
 #svaki svoj priority queue
 
-def pingpong(pid, connread, connwrite, numiter):
+def pingpong(pid, connread, connwrite, numiter, duration):
     '''Creates two processes and repeatedly calls esach other'''
     for iteration in xrange(numiter):
         if pid != 1 or iteration != 0:
             connread[0].recv()
         print 'I am process %d' % pid
+        time.sleep(duration)
         connwrite[0].send('Done')
 
 def parse_args():
     """Parses arguments"""
-    #vidjeti kako ubaciti type int u add_argument
     #zadati vrijeme trajanja procesa, sleep unutar procesa
     parser = argparse.ArgumentParser()
     parser.add_argument('n_iter',
-                        help='number of iteration')
+                        help='number of iteration',
+                        type=int)
     parser.add_argument('n_processes',
-                        help='number of processes')
+                        help='number of processes',
+                        type=int)
     parser.add_argument('method',
                         help='mutual exclusion method')
+    parser.add_argument('process_duration',
+                        help='Duration of each process',
+                        type=float)
     method = parser.parse_args().method
     if method == 'pingpong' or method == 'ricart_argawala':
         pass
     else:
         raise ValueError('Unsupported method. Please choose pingpong or ricart_argawala')
-    return (int(parser.parse_args().n_iter),
-            int(parser.parse_args().n_processes),
-            parser.parse_args().method
+    return (parser.parse_args().n_iter,
+            parser.parse_args().n_processes,
+            parser.parse_args().method,
+            parser.parse_args().process_duration
            )
 
 def main():
     """Main function"""
-    numiter, numprocesses, method = parse_args()
+    numiter, numprocesses, method, duration = parse_args()
     pipesread, pipeswrite = create_pipes(numprocesses, method)
-    processes = create_processes(numiter, numprocesses, method, pipesread, pipeswrite)
+    processes = create_processes(numiter, numprocesses, method, pipesread, pipeswrite, duration)
     print 'Starting processes'
     for proc in processes:
         proc.start()
     for proc in processes:
         proc.join()
-      #zatvoriti sve pipes
+    #map(lambda val: val.close(), [val for key in pipesread for val in key])
+    #map(lambda val: val.close(), [val for key in pipeswrite for val in key])
+    #for conn in pipeswrite:
+     #   conn.close()
+      #zatvoriti sve pipes TODO
     #parent_conn.close()
     #child_conn.close()
     print 'Pipe closed'
