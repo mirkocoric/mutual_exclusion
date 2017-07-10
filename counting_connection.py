8f5be9b20fb66548a03f0a38ea8751102fa66c2d
Mirko Coric <mirkocoric@gmail.com>
2017-07-10 20:32:01 +0200

    Added ObjectProxy

--- counting_connection.py
+++ counting_connection.py
@@ -1,36 +1,35 @@
+from wrapt import ObjectProxy
+
 REQUESTFLAG = 'Request'
 RESPONSEFLAG = 'Response'
 RELEASEFLAG = 'Release'
 
 
-class CountingConnection(object):
+class CountingConnection(ObjectProxy):
     """Class which counts messages and calculates relevant information"""
 
     def __init__(self, conn):
-        self.conn = conn
-        self.analytics = None
-        not_copy = ['__class__', 'send', 'recv']
-        for attr in dir(conn):
-            if attr not in not_copy:
-                setattr(self, attr, getattr(conn, attr))
+        ObjectProxy.__init__(self, conn)
+        self._self_conn = conn
+        self._self_analytics = None
 
     def set_analytics(self, analytics):
         """Sets analytics object"""
-        self.analytics = analytics
+        self._self_analytics = analytics
 
     def send(self, message):
         """Sends message and call functions to count number of messages"""
-        self.conn.send(message)
+        self._self_conn.send(message)
         if message.flag == REQUESTFLAG:
-            self.analytics.send_req()
+            self._self_analytics.send_req()
         elif message.flag == RESPONSEFLAG:
-            self.analytics.send_resp()
+            self._self_analytics.send_resp()
 
     def recv(self):
         """Receives message and call functions to count number of messages"""
-        message = self.conn.recv()
+        message = self._self_conn.recv()
         if message.flag == REQUESTFLAG:
-            self.analytics.recv_req()
+            self._self_analytics.recv_req()
         elif message.flag == RESPONSEFLAG:
-            self.analytics.recv_resp()
+            self._self_analytics.recv_resp()
         return message
